dbc32bd3663f64b91943511d6c67a5bd536103f5
kwkim <kwkim@sanhait.co.kr>
2022-05-09 17:29:33 +0900

    Initial commit

---
+++ src/test/java/com/example/tdd/IntegrationTest.java
@@ -0,0 +1,25 @@
+package com.example.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testName() throws Exception {
+        // arrange
+
+        // act
+
+        // assert
+    }
+}
