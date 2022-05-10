9f16f1d3f3e307300f44b049fd46408fc76c6ea4
kwkim <kwkim@sanhait.co.kr>
2022-05-10 10:39:49 +0900

    Controller 테스트 작성 - CarController class에 [GET]/cars/{name} 생성

--- src/main/java/com/example/tdd/controller/CarController.java
+++ src/main/java/com/example/tdd/controller/CarController.java
@@ -1,4 +1,15 @@
 package com.example.tdd.controller;
 
+import com.example.tdd.domain.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+    @GetMapping("/cars/{name}")
+    private Car getCar(@PathVariable String name) {
+        return null;
+    }
+
 }
