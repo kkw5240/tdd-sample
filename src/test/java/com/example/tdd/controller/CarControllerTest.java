ea842394a265a6c1edb55edf2388b3765c64c59c
kwkim <kwkim@sanhait.co.kr>
2022-05-10 10:35:32 +0900

    Controller 테스트 작성 - MockMvc를 통하여 URL 호출 - HTTP Status Code 200 OK 및 예상 값 테스트 작성

--- src/test/java/com/example/tdd/controller/CarControllerTest.java
+++ src/test/java/com/example/tdd/controller/CarControllerTest.java
@@ -2,14 +2,27 @@ package com.example.tdd.controller;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(CarController.class)
 public class CarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void getCar_ShouldReturnCar() throws Exception {
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/cars/prius"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("prius"))
+                .andExpect(jsonPath("type").value("hybrid"));
     }
 }
