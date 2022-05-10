4e3e71ddfdda486aa524f7d01e5459053e20dfd7
kwkim <kwkim@sanhait.co.kr>
2022-05-10 21:18:40 +0900

    Controller 테스트 작성 - CarController class 일부 작성

--- src/main/java/com/example/tdd/controller/CarController.java
+++ src/main/java/com/example/tdd/controller/CarController.java
@@ -1,15 +1,21 @@
 package com.example.tdd.controller;
 
 import com.example.tdd.domain.Car;
+import com.example.tdd.service.CarService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class CarController {
+
+    private final CarService carService;
+
     @GetMapping("/cars/{name}")
     private Car getCar(@PathVariable String name) {
-        return null;
+        return carService.getCarDetails(name);
     }
 
 }
